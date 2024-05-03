73caddebdb8ef14d661967627da8849053635e41
Shahbaz <167298370+Shahbazabro044@users.noreply.github.com>
2024-05-03 23:32:37 +0500

    Add files via upload

---
+++ COMPARSION OPERATOR/main.ts
@@ -0,0 +1,18 @@
+//comparsion opertors
+//let a = 8 
+//let b = 7 
+//console.log(a==b)
+
+//let a = 5 
+//let b = 7 
+//console.log (a!==b)
+
+ let a = 89
+let b = 78 
+let c = 63
+console.log((a>c) || (b<a) || (c>b))
+
+let d = 72
+let e = 74
+let f = 21
+console.log((d<e)||(f<e)||(f>e))
